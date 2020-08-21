aa943eadf95d82a58bfdfa8bf708c48eaf15471d
madhumithaa <madhumithaa1411@gmail.com>
2020-08-21 18:58:17 +0530

    started sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./MajorComponents/Header";
+import Sidebar from "./MajorComponents/Sidebar";
 import Footer from "./MajorComponents/Footer";
 
 export default class App extends Component {
@@ -8,6 +9,14 @@ export default class App extends Component {
     return (
       <div>
         <Header />
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8">hello</div>
+            <div className="col-lg-4">
+              <Sidebar />
+            </div>
+          </div>
+        </div>
         <Footer />
       </div>
     );
